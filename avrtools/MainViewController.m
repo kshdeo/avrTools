b6cc1f6f9b3ef73037989ce37e260f27678570db
Kshitij Deo <kshdeo@gmail.com>
2013-12-27 02:17:42 +0530

    Dynamically changing baud with frequency etc ..

--- avrtools/MainViewController.m
+++ avrtools/MainViewController.m
@@ -29,13 +29,19 @@
     self.baudRateTextBox.delegate=self;
     
     frequencyArray=[NSArray arrayWithObjects:@"1",@"1.8432",@"2",@"3.6864",@"4",@"7.3728",@"8",@"11.0592",@"12",@"14.7456",@"16",@"18.432",@"20", nil];
-    self.clockFrequencySlider.maximumValue=[frequencyArray count];
+    self.clockFrequencySlider.maximumValue=[frequencyArray count]-1;
     
-    baudArray=[NSArray arrayWithObjects:@"1200",@"2400",@"4800",@"9600",@"14400",@"19200",@"28800",@"38400",@"57600",@"62500",@"76800",@"115200",@"125000",@"230400",@"250000",@"460800",@"500000",@"921000",@"1000000",@"1152000",@"1250000",@"1843200",@"2000000",@"2304000",@"2500000" ,nil];
+    baudArray=[NSArray arrayWithObjects:@"1200",@"2400",@"4800",@"9600",@"14400",@"19200",@"28800",@"38400",@"57600",@"76800",@"115200",@"230400",@"250000",@"460800",@"500000",@"921000",@"1000000",@"1152000",@"1250000",@"1843200",@"2000000",@"2304000",@"2500000" ,nil];
     self.baudRateSlider.maximumValue=[baudArray count]-1;
     
     UIImage *toolTipsImage = [UIImage imageNamed:@"textbox_bg.png"];
     self.backgroundImage.image = [toolTipsImage resizableImageWithCapInsets:UIEdgeInsetsMake(12.0f, 12.0f, 12.0f, 12.0f)];
+}
+
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [self updateConfigs];
 }
 
 
@@ -51,58 +57,66 @@
     return YES;
 }
 
+-(void)textFieldDidEndEditing:(UITextField *)textField
+{
+    [self updateConfigs];
+}
 
 - (IBAction)moreButtonPressed:(id)sender
 {
     if (up)
     {
-        [UIView animateWithDuration:2.0
+        [UIView animateWithDuration:0.8
                          animations:^{
                              self.outputView.frame=CGRectMake(0, 480, 320, 300);
-                             self.moreButton.frame=CGRectMake(290, 484, 22, 22);
+                             self.moreButton.frame=CGRectMake(290, 484, 26, 26);
                          }];
+        [self.moreButton setImage:[UIImage imageNamed:@"arrow-up.png"] forState:UIControlStateNormal];
         up=FALSE;
     }
     else
     {
         up=TRUE;
         
-        [UIView animateWithDuration:2.0
+        [UIView animateWithDuration:0.8
                          animations:^{
-                             self.outputView.frame=CGRectMake(0, 306, 320, 300);
-                             self.moreButton.frame=CGRectMake(290, 310, 22, 22);
+                             self.outputView.frame=CGRectMake(0, 310, 320, 300);
+                             self.moreButton.frame=CGRectMake(290, 314, 26, 26);
                          }];
+        [self.moreButton setImage:[UIImage imageNamed:@"arrow.png"] forState:UIControlStateNormal];
     }
 }
 
+
 - (IBAction)clockFrequencyChanged:(id)sender
 {
-    int stepSize =1;
-    float newStep = roundf((self.clockFrequencySlider.value) / stepSize);
-    
-    self.clockFrequencySlider.value = newStep * stepSize;
-    int val = self.clockFrequencySlider.value;
-    
-    self.clockTextBox.text=[NSString stringWithFormat:@"%3f",[[frequencyArray objectAtIndex:val] floatValue]];
+    [self updateConfigs];
+
 }
 
 
 - (IBAction)baudRateChanged:(id)sender
 {
-    int stepSize =1;
-    float newStep = roundf((self.baudRateSlider.value) / stepSize);
-    
-    self.baudRateSlider.value = newStep * stepSize;
-    int val = self.baudRateSlider.value;
-    
-    self.baudRateTextBox.text=[NSString stringWithFormat:@"%d",[[baudArray objectAtIndex:val] integerValue]];
-    
+
     [self updateConfigs];
 }
 
 
 -(void)updateConfigs
 {
+    int stepSize =1;
+    
+    float newStep = roundf((self.baudRateSlider.value) / stepSize);
+    self.baudRateSlider.value = newStep * stepSize;
+    int val = self.baudRateSlider.value;
+    self.baudRateTextBox.text=[NSString stringWithFormat:@"%d",[[baudArray objectAtIndex:val] integerValue]];
+    
+    newStep = roundf((self.clockFrequencySlider.value) / stepSize);
+    self.clockFrequencySlider.value = newStep * stepSize;
+    val = self.clockFrequencySlider.value;
+    self.clockTextBox.text=[NSString stringWithFormat:@"%3f",[[frequencyArray objectAtIndex:val] floatValue]];
+    
+    
     double baudF = [self.clockTextBox.text floatValue]*1000000/(16*[self.baudRateTextBox.text floatValue]) - 1;
     NSInteger baudI = [self.clockTextBox.text floatValue]*1000000/(16*[self.baudRateTextBox.text floatValue]) - 1;
     
@@ -115,12 +129,19 @@
     double actual = [self.clockTextBox.text floatValue]*1000000/(16*(baudI+1));
     double target = [self.baudRateTextBox.text floatValue];
     double error =100*(actual/target-1);
-    if (baudI==0) error=0;
 
-    self.errorLabel.text = [NSString stringWithFormat:@"%2.1f%%",error];
+    if (error>20 || error<-20)
+    {
+        self.baudRateTextBox.textColor=[UIColor redColor];
+    }
+    else
+    {
+        self.baudRateTextBox.textColor=[UIColor blackColor];
+    }
     
-    NSLog(@"%d,%3f,%3f",baudI,baudF,error);
+    self.errorLabel.text = [NSString stringWithFormat:@"%2.1f%%",error];
 
+    NSLog(@"actual = %3f,  target = %3f, error = %3f",actual,target,error);
 }
 
 
